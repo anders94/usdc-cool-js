59c021d92a28c844f56655d3d964d6749504c450
Matt Venables <matt@venabl.es>
2021-12-08 11:24:38 -0500

    Add prepublish task

---
+++ bin/cli.ts
@@ -0,0 +1,31 @@
+import { fetchTotalSupply } from "../lib"
+
+const main = async () => {
+  const {
+    algorandSupply,
+    ethereumSupply,
+    hederaSupply,
+    solanaSupply,
+    stellarSupply,
+    tronSupply,
+    totalSupply
+  } = await fetchTotalSupply()
+
+  console.log(`Algorand: $${algorandSupply.toString()}`)
+  console.log(`Ethereum: $${ethereumSupply.toString()}`)
+  console.log(`Hedera: $${hederaSupply.toString()}`)
+  console.log(`Solana: $${solanaSupply.toString()}`)
+  console.log(`Stellar: $${stellarSupply.toString()}`)
+  console.log(`Tron: $${tronSupply.toString()}`)
+  console.log("----------")
+  console.log(`Total: $${totalSupply.toString()}`)
+}
+
+main()
+  .then(() => {
+    process.exit(0)
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
