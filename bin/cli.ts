ac3701c08ac815c9fdf5afb57d2c6e3d2da8d029
Anders Brownworth <anders-git@evantide.com>
2022-01-25 22:00:46 -0500

    USD number formatting

--- bin/cli.ts
+++ bin/cli.ts
@@ -13,15 +13,20 @@ const main = async () => {
     totalSupply
   } = await fetchTotalSupply()
 
-  console.log(`Algorand: $${algorandSupply.toString()}`)
-  console.log(`Avalanche: $${avalancheSupply.toString()}`)
-  console.log(`Ethereum: $${ethereumSupply.toString()}`)
-  console.log(`Hedera: $${hederaSupply.toString()}`)
-  console.log(`Solana: $${solanaSupply.toString()}`)
-  console.log(`Stellar: $${stellarSupply.toString()}`)
-  console.log(`Tron: $${tronSupply.toString()}`)
-  console.log("----------")
-  console.log(`Total: $${totalSupply.toString()}`)
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  console.log(` Algorand: ${formatter.format(algorandSupply.toNumber()).padStart(20)}`)
+  console.log(`Avalanche: ${formatter.format(avalancheSupply.toNumber()).padStart(20)}`)
+  console.log(` Ethereum: ${formatter.format(ethereumSupply.toNumber()).padStart(20)}`)
+  console.log(`   Hedera: ${formatter.format(hederaSupply.toNumber()).padStart(20)}`)
+  console.log(`   Solana: ${formatter.format(solanaSupply.toNumber()).padStart(20)}`)
+  console.log(`  Stellar: ${formatter.format(stellarSupply.toNumber()).padStart(20)}`)
+  console.log(`     Tron: ${formatter.format(tronSupply.toNumber()).padStart(20)}`)
+  console.log("-------------------------------")
+  console.log(`    Total: ${formatter.format(totalSupply.toNumber()).padStart(20)}`)
 }
 
 main()
